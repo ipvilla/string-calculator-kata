df9ea8a0137752ba4d59534bcc8a763823379b4d
Isa Pérez <isperez@aidacanarias.com>
2022-03-25 16:35:14 +0100

    Little refactor in the test class

--- StringCalculatorSpecs/StringCalculatorShould.cs
+++ StringCalculatorSpecs/StringCalculatorShould.cs
@@ -26,7 +26,7 @@ namespace StringCalculatorSpecs
         [Test]
         public void return_1_as_a_number_when_input_string_is_1()
         {
-            var inputString = "1";
+            const string inputString = "1";
 
             var result = _stringCalculator.Add(inputString);
 
@@ -36,7 +36,7 @@ namespace StringCalculatorSpecs
         [Test]
         public void return_2_as_a_number_when_input_string_is_2()
         {
-            var inputString = "2";
+            const string inputString = "2";
 
             var result = _stringCalculator.Add(inputString);
 

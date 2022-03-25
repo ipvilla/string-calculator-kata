66896a49eb8708fcbb012951194d7d3b7eee7205
Isa Pérez <isperez@aidacanarias.com>
2022-03-25 16:26:57 +0100

    New test in case there's only one number in the string

--- StringCalculatorSpecs/StringCalculatorShould.cs
+++ StringCalculatorSpecs/StringCalculatorShould.cs
@@ -15,5 +15,16 @@ namespace StringCalculatorSpecs
 
             result.Should().Be(0);
         }
+
+        [Test]
+        public void return_string_as_a_number_when_input_string_is_only_one_number()
+        {
+            var inputString = "1";
+            var stringCalculator = new StringCalculator.StringCalculator();
+
+            var result = stringCalculator.Add(inputString);
+
+            result.Should().Be(1);
+        }
     }
 }

9eb2b8fdbb0f3bdf368c88fe83f917ce04c7bf81
Isa Pérez <isperez@aidacanarias.com>
2022-03-28 18:34:32 +0200

    Fixed test for the case of two numbers in string separated by comma

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -8,6 +8,11 @@ namespace StringCalculator
             {
                 return 0;
             }
+            if (inputString.Contains(","))
+            {
+                var numbers = inputString.Split(",");
+                return int.Parse(numbers[0]) + int.Parse(numbers[1]);
+            }
             return int.Parse(inputString);
         }
     }

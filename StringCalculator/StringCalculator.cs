f9664ba6fcc612cb4713c2ab157bd628b5f8563c
Isa Pérez <isperez@aidacanarias.com>
2022-03-28 18:28:10 +0200

    Set test of empty input string to green

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -4,6 +4,10 @@ namespace StringCalculator
     {
         public int Add(string inputString)
         {
+            if (inputString == "")
+            {
+                return 0;
+            }
             return int.Parse(inputString);
         }
     }

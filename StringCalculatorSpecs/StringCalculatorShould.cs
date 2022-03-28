f22b65697a624cc0e8f153ad489cdd8b8e63cbc9
Isa Pérez <isperez@aidacanarias.com>
2022-03-28 18:23:10 +0200

    New Test for the case of only having one number in the string

--- StringCalculatorSpecs/StringCalculatorShould.cs
+++ StringCalculatorSpecs/StringCalculatorShould.cs
@@ -1,3 +1,4 @@
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace StringCalculatorSpecs
@@ -10,9 +11,21 @@ namespace StringCalculatorSpecs
         }
 
         [Test]
-        public void Test1()
+        public void return_number_when_only_one_number_in_input_string()
         {
-            Assert.Pass();
+            var stringCalculator = new StringCalculator();
+
+            var result = stringCalculator.Add("1");
+
+            result.Should().Be(1);
+        }
+    }
+
+    public class StringCalculator
+    {
+        public int Add(string inputString)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }

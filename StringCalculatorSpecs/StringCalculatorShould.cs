24d81366147278e69bf8e45a01a6a5bce64d6765
Isa Pérez <isperez@aidacanarias.com>
2022-04-01 16:06:24 +0200

    ✅ return_input_string_as_number_when_input_string_is_only_one_number

--- StringCalculatorSpecs/StringCalculatorShould.cs
+++ StringCalculatorSpecs/StringCalculatorShould.cs
@@ -34,7 +34,9 @@ namespace StringCalculatorSpecs
     {
         public int Add(string numbers)
         {
-            return 0;
+            if (string.IsNullOrWhiteSpace(numbers))
+                return 0;
+            return int.Parse(numbers);
         }
     }
 }
